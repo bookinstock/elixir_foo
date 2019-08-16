d8bfbfda93fd77aa2e7d16e7faa6ab822173889a
Wende Lu <wendelu1991@gmail.com>
2019-08-16 18:03:29 +0800

    Format GenServer try

--- lib/foo/gen_server.ex
+++ lib/foo/gen_server.ex
@@ -7,11 +7,15 @@
 # 开启和关闭灯操作(Start, Terminate)
 
 # 通常客户端的调用方法和服务端相应方法会写在一个模块中
+# 推荐同一个模块中的客户端和服务端代码也是要分开的~
 # 但当服务中的逻辑过于复杂和代码过多的时候，可以分开写
 
 defmodule Foo.GenServer do
-  use GenServer
+  use GenServer, restart: :transient, shutdown: 10_000
 
+  # 选项 :name 用来制定注册的名字(Process.register/2)
+  # 注意！若 Server 是动态创建的，不要使用 atom 作为名字
+  # 因为 atom 是不会进行垃圾回收的
   def start_link(state \\ []) do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
@@ -28,6 +32,10 @@ defmodule Foo.GenServer do
     GenServer.call(__MODULE__, :state)
   end
 
+  def from do
+    GenServer.call(__MODULE__, :from)
+  end
+
   def init(state) do
     {:ok, state}
   end
@@ -41,13 +49,15 @@ defmodule Foo.GenServer do
   end
 
   def handle_call(:pop, from, [head | tail]) do
-    IO.puts "!"
     IO.inspect(from)
-    IO.puts "!-"
     {:reply, head, tail}
   end
 
   def handle_call(:state, _from, state) do
     {:reply, state, state}
+  end
+
+  def handle_call(:from, from, state) do
+    {:reply, from, state}
   end
 end
--- test/foo/gen_server_test.exs
+++ test/foo/gen_server_test.exs
@@ -10,20 +10,20 @@ defmodule Foo.GenServerTest do
   test "Init stack with default state" do
     Foo.GenServer.start_link()
 
-    assert Foo.GenServer.state == []
+    assert Foo.GenServer.state() == []
   end
 
   test "Init stack with [1, 2, 3] state" do
     Foo.GenServer.start_link([1, 2, 3])
 
-    assert Foo.GenServer.state == [1, 2, 3]
+    assert Foo.GenServer.state() == [1, 2, 3]
   end
 
   test "Push element to stack" do
     Foo.GenServer.start_link([1, 2, 3])
     Foo.GenServer.push(10)
 
-    assert Foo.GenServer.state == [10, 1, 2, 3]
+    assert Foo.GenServer.state() == [10, 1, 2, 3]
   end
 
   test "Pop element from stack" do
@@ -31,7 +31,7 @@ defmodule Foo.GenServerTest do
     element = Foo.GenServer.pop()
 
     assert element == 1
-    assert Foo.GenServer.state == [2, 3]
+    assert Foo.GenServer.state() == [2, 3]
   end
 
   test "Pop element from empty stack" do
@@ -39,14 +39,44 @@ defmodule Foo.GenServerTest do
     element = Foo.GenServer.pop()
 
     assert element == nil
-    assert Foo.GenServer.state == []
+    assert Foo.GenServer.state() == []
   end
 
   test "Child spec" do
     assert Foo.GenServer.child_spec([]) ==
-      %{
-        id: Foo.GenServer,
-        start: {Foo.GenServer, :start_link, [[]]}
-      }
+             %{
+               id: Foo.GenServer,
+               start: {Foo.GenServer, :start_link, [[]]},
+               restart: :transient,
+               shutdown: 10000
+             }
+  end
+
+  test "Show from" do
+    Foo.GenServer.start_link()
+    {pid, ref} = Foo.GenServer.from()
+
+    assert is_pid(pid) == true
+    assert is_reference(ref) == true
+  end
+
+  test "Start from superviosr" do
+    children = [
+      Foo.GenServer
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_all)
+
+    assert Foo.GenServer.state() == []
+  end
+
+  test "Start from superviosr with state" do
+    children = [
+      {Foo.GenServer, [1, 2, 3]}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_all)
+
+    assert Foo.GenServer.state() == [1, 2, 3]
   end
 end
