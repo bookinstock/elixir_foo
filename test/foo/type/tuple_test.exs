8de1e58b36518ccc811e38783d0871bddbd456a1
Wende Lu <wendelu1991@gmail.com>
2019-09-03 14:55:56 +0800

    Add notes

--- test/foo/type/tuple_test.exs
+++ test/foo/type/tuple_test.exs
@@ -1,22 +1,27 @@
 defmodule TupleTest do
   use ExUnit.Case, async: true
 
-  test "append" do
-    assert Tuple.append({"foo"}, "bar") == {"foo", "bar"}
-  end
-
-  test "insert_at" do
-    assert Tuple.insert_at({"a", "b"}, 1, "foo") == {"a", "foo", "b"}
-  end
-
-  test "delete_at" do
-    assert Tuple.delete_at({"a", "foo", "b"}, 1) == {"a", "b"}
-  end
-
+  # create
   test "duplicate" do
     assert Tuple.duplicate("foo", 3) == {"foo", "foo", "foo"}
   end
 
+  # add
+  test "append" do
+    assert Tuple.append({"foo"}, "bar") == {"foo", "bar"}
+  end
+
+  # add
+  test "insert_at" do
+    assert Tuple.insert_at({"a", "b"}, 1, "foo") == {"a", "foo", "b"}
+  end
+
+  # delete
+  test "delete_at" do
+    assert Tuple.delete_at({"a", "foo", "b"}, 1) == {"a", "b"}
+  end
+
+  # convert
   test "convert to list" do
     assert Tuple.to_list({"foo", "bar"}) == ["foo", "bar"]
   end
