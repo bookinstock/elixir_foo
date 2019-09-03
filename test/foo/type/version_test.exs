2e9d1938329b03a1cd03c676c21b98253c83bce7
Wende Lu <wendelu1991@gmail.com>
2019-09-03 11:46:23 +0800

    Change code order

--- test/foo/type/version_test.exs
+++ test/foo/type/version_test.exs
@@ -9,6 +9,23 @@ defmodule VersionTest do
   #   pre: pre()
   # }
 
+  test "parse version" do
+    assert {:ok, version} = Version.parse("1.2.3-foo.bar")
+    assert %Version{} = version
+    assert version.major == 1
+    assert version.minor == 2
+    assert version.patch == 3
+    assert version.pre == ["foo", "bar"]
+
+    assert Version.parse("foobar") == :error
+  end
+
+  test "parse requirement" do
+    assert {:ok, version_requirement} = Version.parse_requirement("~> 1.1.1")
+    assert %Version.Requirement{} = version_requirement
+    assert version_requirement.source == "~> 1.1.1"
+  end
+
   test "compare versions" do
     assert Version.compare(Version.parse!("1.1.1"), Version.parse!("1.1.1")) == :eq
     assert Version.compare("1.1.1", "1.1.1") == :eq
@@ -25,22 +42,5 @@ defmodule VersionTest do
     refute Version.match?("1.2.0", "~> 1.1.0")
     assert Version.match?("1.1.5-beta", "~> 1.1.0")
     refute Version.match?("1.1.5-beta", "~> 1.1.0", allow_pre: false)
-  end
-
-  test "parse version" do
-    assert {:ok, version} = Version.parse("1.2.3-foo.bar")
-    assert %Version{} = version
-    assert version.major == 1
-    assert version.minor == 2
-    assert version.patch == 3
-    assert version.pre == ["foo", "bar"]
-
-    assert Version.parse("foobar") == :error
-  end
-
-  test "parse requirement" do
-    assert {:ok, version_requirement} = Version.parse_requirement("~> 1.1.1")
-    assert %Version.Requirement{} = version_requirement
-    assert version_requirement.source == "~> 1.1.1"
   end
 end
